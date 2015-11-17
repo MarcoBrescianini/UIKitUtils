2a2708647a98f604d382de743407187d1a1f921b
Marco <marco.brescianini@therope.it>
2015-11-17 12:28:21 +0100

    Initial Commit

---
+++ UIKitUtils/UIKitUtils.h
@@ -0,0 +1,19 @@
+//
+//  UIKitUtils.h
+//  UIKitUtils
+//
+//  Created by Engineering Solutions on 17/11/15.
+//  Copyright © 2015 Engineering Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitUtils.
+FOUNDATION_EXPORT double UIKitUtilsVersionNumber;
+
+//! Project version string for UIKitUtils.
+FOUNDATION_EXPORT const unsigned char UIKitUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitUtils/PublicHeader.h>
+
+
